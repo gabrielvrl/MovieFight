7dea7a3c085f1b93667b7fe8d69374af3aaf1cdf
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2021-11-28 16:18:36 -0300

    Fetching Movie Data

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hi there!');
+const fetchData = async () => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey: '4258e9f1',
+      s: 'avengers'
+    }
+  });
+
+  console.log(response.data)
+}
+
+fetchData();
