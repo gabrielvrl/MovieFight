149979ce98279165d7d60c9534076a66eb047fb2
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2021-11-29 07:41:31 -0300

    Searching the API on Input Change

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-const fetchData = async () => {
+const fetchData = async (searchTerm) => {
   const response = await axios.get('http://www.omdbapi.com/', {
     params: {
       apikey: '4258e9f1',
-      i: 'tt0848228'
+      s: searchTerm
     }
   });
 
   console.log(response.data)
-}
+};
 
-fetchData();
+const input = document.querySelector('input');
+input.addEventListener('input', (event) => {
+  fetchData(event.target.value);
+});
