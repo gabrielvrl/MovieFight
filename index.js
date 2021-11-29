68d07bb8730f034118c6653593ac1f5ce981b020
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2021-11-29 08:23:44 -0300

    Rendering Movies

--- index.js
+++ index.js
@@ -13,6 +13,16 @@ const input = document.querySelector('input');
 
 const onInput = async event => {
   const movies = await fetchData(event.target.value);
-  console.log(movies)
+  
+  for(let movie of movies){
+    const div = document.createElement('div');
+
+    div.innerHTML = `
+      <img src="${movie.Poster}" />
+      <h1>${movie.Title}</h1>
+    `;
+
+    document.querySelector('#target').appendChild(div)
+  }
 };
 input.addEventListener('input', debounce(onInput, 500));
