a54ced676b415a6c1195c192b0fe7e46076bcca0
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2021-11-30 07:31:45 -0300

    Handling Broken Images

--- index.js
+++ index.js
@@ -31,13 +31,15 @@ const resultsWrapper = document.querySelector('.results');
 const onInput = async event => {
   const movies = await fetchData(event.target.value);
 
+  resultsWrapper.innerHTML = '';
   dropdown.classList.add('is-active');
   for (let movie of movies) {
     const option = document.createElement('a');
+    const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
 
     option.classList.add('dropdown-item');
     option.innerHTML = `
-      <img src="${movie.Poster}" />
+      <img src="${imgSrc}" />
       ${movie.Title}
     `;
 
