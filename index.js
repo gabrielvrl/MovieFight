5a4b419d2e4264c09663dca66a3c173b15aa0bb6
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2021-11-30 07:42:36 -0300

    Handling Movie Selection

--- index.js
+++ index.js
@@ -47,6 +47,10 @@ const onInput = async event => {
       <img src="${imgSrc}" />
       ${movie.Title}
     `;
+    option.addEventListener('click', () => {
+      dropdown.classList.remove('is-active');
+      input.value = movie.Title;
+    });
 
     resultsWrapper.appendChild(option);
   }
