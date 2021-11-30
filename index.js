ea78f60fdc4424c3b6ffa60b1744a85a37250c36
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2021-11-30 07:59:06 -0300

    Making a Followup Request

--- index.js
+++ index.js
@@ -50,6 +50,7 @@ const onInput = async event => {
     option.addEventListener('click', () => {
       dropdown.classList.remove('is-active');
       input.value = movie.Title;
+      onMovieSelect(movie);
     });
 
     resultsWrapper.appendChild(option);
@@ -62,3 +63,13 @@ document.addEventListener('click', event => {
     dropdown.classList.remove('is-active')
   }
 })
+
+const onMovieSelect = async (movie) => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey: '4258e9f1',
+      i: movie.imdbID
+    }
+  })
+  console.log(response.data)
+}
