14a3def869a7e6993700d8def9d41a60d4c09685
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2021-11-30 07:24:38 -0300

    Repairing References

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const root = document.querySelector('.autocomplete');
 root.innerHTML = `
   <label><b>Search For a Movie</b></label>
   <input class="input" />
-  <div class="dropdown>
+  <div class="dropdown">
     <div class="dropdown-menu">
       <div class="dropdown-content results"></div>
     </div>
@@ -26,20 +26,22 @@ root.innerHTML = `
 
 const input = document.querySelector('input');
 const dropdown = document.querySelector('.dropdown');
-const resultsWrapper = document.querySelector('.results')
+const resultsWrapper = document.querySelector('.results');
 
 const onInput = async event => {
   const movies = await fetchData(event.target.value);
-  
-  for(let movie of movies){
-    const div = document.createElement('div');
 
-    div.innerHTML = `
+  dropdown.classList.add('is-active');
+  for (let movie of movies) {
+    const option = document.createElement('a');
+
+    option.classList.add('dropdown-item');
+    option.innerHTML = `
       <img src="${movie.Poster}" />
-      <h1>${movie.Title}</h1>
+      ${movie.Title}
     `;
 
-    document.querySelector('#target').appendChild(div)
+    resultsWrapper.appendChild(option);
   }
 };
 input.addEventListener('input', debounce(onInput, 500));
