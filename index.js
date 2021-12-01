0cd2d73c0f39b99bfc812a8338c26dbc9b93c47e
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2021-12-01 07:35:11 -0300

    Avoiding Duplication of COnfig

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-createAutoComplete({
-  root: document.querySelector('.autocomplete'),
+const autoCompleteConfig = {
   renderOption(movie) {
     const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
     return `
@@ -27,6 +26,16 @@ createAutoComplete({
   
     return response.data.Search;
   }
+}
+
+createAutoComplete({
+  ...autoCompleteConfig,
+  root: document.querySelector('#left-autocomplete')
+});
+
+createAutoComplete({
+  ...autoCompleteConfig,
+  root: document.querySelector('#right-autocomplete')
 });
 
 const onMovieSelect = async (movie) => {
