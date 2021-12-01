e29db6ea3d6e04158da580456e0cadc7a4cd2a09
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2021-12-01 07:21:46 -0300

    Removing Movie References

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-const fetchData = async (searchTerm) => {
-  const response = await axios.get('http://www.omdbapi.com/', {
-    params: {
-      apikey: '4258e9f1',
-      s: searchTerm
-    }
-  });
-
-  if(response.data.Error) {
-    return [];
-  }
-
-  return response.data.Search;
-};
-
 createAutoComplete({
   root: document.querySelector('.autocomplete'),
   renderOption(movie) {
@@ -27,6 +12,20 @@ createAutoComplete({
   },
   inputValue(movie) {
     return movie.Title;
+  },
+  async fetchData(searchTerm) {
+    const response = await axios.get('http://www.omdbapi.com/', {
+      params: {
+        apikey: '4258e9f1',
+        s: searchTerm
+      }
+    });
+  
+    if(response.data.Error) {
+      return [];
+    }
+  
+    return response.data.Search;
   }
 });
 
